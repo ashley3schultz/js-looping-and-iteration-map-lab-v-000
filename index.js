bde18c8fb59bc1fb3c00122336f411f9c8ef4fab
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-26 21:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function lowerCaseDrivers(arr) {
   return arr.map(function (name){return name.toLowerCase()})
 }
 
-function nameToAttributes(obj) {
-  return arr.map(function (name){ return Object.assign({}, name, {firstName: name.first, lastName: name.last}) })
+function nameToAttributes(arr) {
+  return arr.map(function (name){ return Object.assign({}, name, {firstName: name.split(" ")[0], lastName: name.last}) })
 }
 
 function attributesToPhrase(obj) {
