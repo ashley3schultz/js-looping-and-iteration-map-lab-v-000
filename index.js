98b887d383318c7cfce9659247d63798cf13571b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-26 21:56:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function lowerCaseDrivers(arr) {
 }
 
 function nameToAttributes(arr) {
-  return arr.map(function (user){ return Object.assign({}, user, {firstName: user.name.split(" ")[0], lastName: user.name.split(" ")[1] }) })
+  return arr.map(function (user){ return Object.assign({}, user, {firstName: user.split(" ")[0], lastName: user.split(" ")[1] }) })
 }
 
 function attributesToPhrase(obj) {
